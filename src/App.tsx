019658a2c76a7e1c83f0551de06acfc905d73795
Alexander Illarionov <ruzinovrun@gmail.com>
2025-02-10 20:43:07 +0100

    basic setup: webpack, routing, scss, typescript...

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to={'/'}>MainPage</Link>
+      <Link to={'/about'}>AboutPage</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
