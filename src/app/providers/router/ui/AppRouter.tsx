96b3b84b24400c4c95b72c64b33c67f561b3a02a
Alexander Illarionov <ruzinovrun@gmail.com>
2025-02-16 11:27:26 +0100

    NotFoundPage, Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,28 +1,24 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
-import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
-const AppRouter = () => {
-  const { t } = useTranslation('appRouter');
-
-  return (
-    <Suspense fallback={<div>{t('loading')}</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={(
-              <Suspense fallback={<div>{t('loading')}</div>}>
-                <div className="page-wrapper">{element}</div>
-              </Suspense>
-            )}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+const AppRouter = () => (
+  <Routes>
+    {Object.values(routeConfig).map(({ element, path }) => (
+      <Route
+        key={path}
+        path={path}
+        element={(
+          <Suspense fallback={<PageLoader />}>
+            <div className="page-wrapper">
+              {element}
+            </div>
+          </Suspense>
+        )}
+      />
+    ))}
+  </Routes>
+);
 
 export default AppRouter;
