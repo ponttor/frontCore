323bfed79b57698a15aaca405078657acf5051a0
Alexander Illarionov <ruzinovrun@gmail.com>
2025-03-20 18:24:52 +0100

    ignore stories in lint:ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     '@typescript-eslint',
     'i18next',
   ],
+  ignorePatterns: ['**/*.stories.tsx'],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
